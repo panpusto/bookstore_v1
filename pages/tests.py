cf40437b0ee81756c06e40028cb9a3f07d78345a
Cezary <102999499+panpusto@users.noreply.github.com>
2022-10-13 16:25:51 +0200

    add tests for about page

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -9,7 +9,7 @@ class HomePageTests(SimpleTestCase):
         url = reverse("home")
         self.response = self.client.get(url)
 
-    def test_urls_exists_at_correct_location(self):
+    def test_homepage_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
@@ -24,3 +24,25 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, "About")
+
+    def test_aboutpage_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Not contain me")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
